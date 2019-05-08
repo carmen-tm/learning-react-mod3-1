63817bbd144ac5328ad9e478265f608beef93238
carmen-tm <carmentorremolina@gmail.com>
2019-05-08 18:38:40 +0200

    Add a solution that works with props in the mather

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import Item from './Item';
 
-const items = [
-	{
-		name: 'Cereales con chocolate',
-		description: 'Cereales rellenos de chocolate',
-		quantity: 2,
-		category: 'Cereales',
-		price: 5
-	},
-	{
-		name: 'Hamburguesa con queso',
-		description: 'Hamburguesa rica y saludable',
-		quantity: 1,
-		category: 'Fast-food',
-		price: 15
-	},
-	{
-		name: 'Agua mineral',
-		description: 'Agua de un charco del Himalaya',
-		quantity: 2,
-		category: 'Bebida',
-		price: 5
-	}
-];
-
 class ItemList extends React.Component {
 	render() {
-		const itemsArr = items.map(function generateItems(item) {
+		console.log(this.props);
+		console.log(this.props.mySweetItems);
+		const itemsArr = this.props.mySweetItems.map(function generateItems(item) {
 			const { name, description, quantity, category, price } = item;
 
 			return (
@@ -48,52 +26,3 @@ class ItemList extends React.Component {
 }
 
 export default ItemList;
-
-//////////////
-
-// function myItems() {
-// 	let myItem = '';
-// 	for (const item of items) {
-// 		const { name, description, quantity, category, price } = item;
-
-// 		myItem = `
-// 			<li>
-// 				<Item
-// 					name=${name}
-// 					description=${description}
-// 					quantity=${quantity}
-// 					category=${category}
-// 					price=${price}
-// 				/>
-// 			</li>`;
-// 		console.log(myItem);
-// 	}
-// 	return myItem;
-// }
-
-// class ItemList extends React.Component {
-// 	render() {
-// 		return (
-// 			<ul className="item-list">
-// 				<li>
-// 					<Item
-// 						name="Cereales con chocolate"
-// 						description="Cereales rellenos de chocolate"
-// 						quantity={2}
-// 						category="Cereales"
-// 						price={5}
-// 					/>
-// 				</li>
-// 				<li>
-// 					<Item
-// 						name="Hamburguesa con queso"
-// 						description="Hamburguesa rica y saludable"
-// 						quantity={1}
-// 						category="Fast-food"
-// 						price={15}
-// 					/>
-// 				</li>
-// 			</ul>
-// 		);
-// 	}
-// }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ItemList from './components/ItemList';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const items = [
+	{
+		name: 'Cereales con chocolate',
+		description: 'Cereales rellenos de chocolate',
+		quantity: 2,
+		category: 'Cereales',
+		price: 5
+	},
+	{
+		name: 'Hamburguesa con queso',
+		description: 'Hamburguesa rica y saludable',
+		quantity: 1,
+		category: 'Fast-food',
+		price: 15
+	},
+	{
+		name: 'Agua mineral',
+		description: 'Agua de un charco del Himalaya',
+		quantity: 2,
+		category: 'Bebida',
+		price: 5
+	}
+];
+
+ReactDOM.render(
+	<ItemList mySweetItems={items} />,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
